e3af325a7531376984e54da1074e70ebcddf7e45
Ayberk Uman <ayberkuman1@gmail.com>
2026-02-12 14:32:08 +0300

    fix: build error

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -44,7 +44,7 @@ export function NavMain({
 					>
 						<SidebarMenuItem>
 							<CollapsibleTrigger asChild>
-								<SidebarMenuButton tooltip={item.title}>
+								<SidebarMenuButton>
 									{item.icon}
 									<span>{item.title}</span>
 									<ChevronRightIcon className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
